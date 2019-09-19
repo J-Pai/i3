1658436902e834e04eafb0ddd1a790216a5d6b05
Jesse Pai <Jesse.Pai@gmail.com>
2019-09-19 15:34:04 -0700

    Removed execution of screen layout script. Wrong place.

--- g-layout.sh
+++ g-layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-7 --off --output DP-6 --mode 2560x1440 --pos 2560x0 --rotate right --output DP-5 --off --output DP-4 --primary --mode 2560x1440 --pos 0x800 --rotate normal --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
+xrandr --output DP-7 --off --output DP-6 --mode 2560x1440 --pos 2560x0 --rotate right --output DP-5 --off --output DP-4 --primary --mode 2560x1440 --pos 0x925 --rotate normal --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
