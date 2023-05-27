ff68557d989799bece33a62e12c58fc07a3ce868
j-pai <jesse.pai@gmail.com>
2023-05-27 01:22:57 -0700

    Updated volume control block for i3blocks.

---
+++ i3blocks/volume-pulseaudio.sh
@@ -0,0 +1,9 @@
+export AUDIO_HIGH_SYMBOL=''
+export AUDIO_MED_THRESH=50
+export AUDIO_MED_SYMBOL=''
+export AUDIO_LOW_THRESH=0
+export AUDIO_LOW_SYMBOL=''
+export AUDIO_MUTED_SYMBOL=''
+export AUDIO_DELTA=1
+export USE_ALSA_NAME=1
+~/.config/i3/i3blocks-contrib/volume-pulseaudio/volume-pulseaudio
