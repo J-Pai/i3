7b053ce48f5bee75467fda667253f2e481610faa
j-pai <jesse.pai@gmail.com>
2023-10-28 16:38:44 -0700

    Found trick to keep default sink active.

---
+++ i3blocks/README.md
@@ -0,0 +1,3 @@
+# pipewire Tricks
+
+https://discussion.fedoraproject.org/t/how-do-i-disable-audio-sink-suspend-on-idle-using-wireplumber-and-pipewire-on-fedora-35-so-that-my-audio-isnt-delayed-when-playback-resumes/69861/2
--- i3blocks/volume-pipewire.sh
+++ i3blocks/volume-pipewire.sh
@@ -4,4 +4,4 @@ export AUDIO_MED_SYMBOL=''
 export AUDIO_LOW_THRESH=0
 export AUDIO_LOW_SYMBOL=''
 export AUDIO_MUTED_SYMBOL=''
-~/.config/i3/i3blocks/volume-pipewire/volume-pipewire
+~/.config/i3/i3blocks-contrib/volume-pipewire/volume-pipewire
