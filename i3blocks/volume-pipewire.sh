3affbf414c5d95e21720f3e1669c49004ccd427a
j-pai <jesse.pai@gmail.com>
2023-10-28 16:02:57 -0700

    Switched to pipewire block for i3blocks.

---
+++ i3blocks/volume-pipewire.sh
@@ -0,0 +1,7 @@
+export AUDIO_HIGH_SYMBOL=''
+export AUDIO_MED_THRESH=50
+export AUDIO_MED_SYMBOL=''
+export AUDIO_LOW_THRESH=0
+export AUDIO_LOW_SYMBOL=''
+export AUDIO_MUTED_SYMBOL=''
+~/.config/i3/i3blocks-contrib/volume-pipewire/volume-pipewire
