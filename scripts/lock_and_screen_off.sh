432fc8f814d227de32ed543ca51404d22915f458
j-pai <jesse.pai@gmail.com>
2023-12-02 16:38:52 -0800

    Added loop to screen off.

--- scripts/lock_and_screen_off.sh
+++ scripts/lock_and_screen_off.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 i3lock $@
-sleep 30
-if [ $(pgrep i3lock | wc -l ) -ne 0 ]; then
-    xset dpms force off
-fi
+while true; do
+    sleep 30
+    if [ $(pgrep i3lock | wc -l ) -ne 0 ]; then
+        xset dpms force off
+    else
+        exit
+    fi
+done
